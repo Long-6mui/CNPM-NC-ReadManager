b909456d2c8d208f6562303319d0f81b4ed2654a
Long 6 mui <thuytinhhoanglong1510@gmail.com>
2026-09-18 14:34:54 +0700

    Setup API Web Mobile solution and organize documents

---
+++ scr/ReadManager.Mobile/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ReadManager.Mobile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ scr/ReadManager.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ReadManager.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
